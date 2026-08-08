37db42b590374f6671534f24cc8b33252e81de98
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-08-08 11:43:24 +0530

    Bubble sort

---
+++ Sorting/bubbleSort.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int>& arr, int n){
+    for(int i = n-1; i>=0; i--){
+        for(int j = 0; j<=i-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+int main(){
+    int n;
+    
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+
+    vector<int>arr(n);
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    bubbleSort(arr, n);
+
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+    
+}
